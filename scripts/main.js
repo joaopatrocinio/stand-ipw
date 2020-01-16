92ffb16e74aee5ad2cdaaefa3d0a217aa158256d
João Patrocínio <jpatrocinio5436@hotmail.com>
2020-01-16 08:55:40 +0000

    carregar carros a partir de um array

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+let carros = [
+    { 
+        "id": 1, 
+        "marca": "Audi", 
+        "modelo": "A3 Sportback 1.6 TDI Advance", 
+        "ano": 2016,
+        "kms": 26000, 
+        "preco": 28900,
+        "imagens": ["imgAudi1.jpg", "imgAudi2.jpg", "imgAudi3.jpg", "imgAudi4.jpg", "imgAudi5.jpg"]
+    },
+    { 
+        "id": 2, 
+        "marca": "Mercedes-Benz", 
+        "modelo": "AMG GT", 
+        "ano": 2018, 
+        "kms": 900, 
+        "preco": 162500,
+        "imagens": ["imgMercedes2.jpg", "imgMercedes.jpg", "imgMercedes3.jpg", "imgMercedes4.jpg", "imgMercedes5.jpg"]
+    },
+    { 
+        "id": 3, 
+        "marca": "Nissan", 
+        "modelo": "Micra Tekna", 
+        "ano": 2017, 
+        "kms": 114000, 
+        "preco": 16900,
+        "imagens": ["Nissan_Micra_tekna.jpg", "imgMicra1.jpg", "imgMicra2.jpg", "imgMicra3.jpg", "imgMicra4.jpg"]
+    },
+    { 
+        "id": 4, 
+        "marca": "Opel", 
+        "modelo": "Astra 1.3 CDTi Selection S/S", 
+        "ano": 2014, 
+        "kms": 82000, 
+        "preco": 11900,
+        "imagens": ["imgAstra2.jpg", "imgAstra.jpg", "imgAstra3.jpg", "imgAstra4.jpg", "imgAstra5.jpg"]
+    }
+];
