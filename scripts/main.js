9162b0faeb763dbc2acc5926c85a4391b45bbbe0
Nelson Filipe Freire Antunes <nantunes@NAMacBook.lan>
2020-01-19 19:05:38 +0000

    Adicionar Bmw

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 let carros = [
-    { 
-        "id": 1, 
-        "marca": "Audi", 
-        "modelo": "A3 Sportback 1.6 TDI Advance", 
+    {
+        "id": 1,
+        "marca": "Audi",
+        "modelo": "A3 Sportback 1.6 TDI Advance",
         "ano": 2016,
-        "kms": 26000, 
+        "kms": 26000,
         "preco": 28900,
         "imagens": ["imgAudi1.jpg", "imgAudi2.jpg", "imgAudi3.jpg", "imgAudi4.jpg", "imgAudi5.jpg"],
         "mes_ano": "07/2016",
@@ -19,12 +19,12 @@ let carros = [
         "portas": 5,
         "lugares": 5
     },
-    { 
-        "id": 2, 
-        "marca": "Mercedes-Benz", 
-        "modelo": "AMG GT", 
-        "ano": 2018, 
-        "kms": 900, 
+    {
+        "id": 2,
+        "marca": "Mercedes-Benz",
+        "modelo": "AMG GT",
+        "ano": 2018,
+        "kms": 900,
         "preco": 162500,
         "imagens": ["imgMercedes2.jpg", "imgMercedes.jpg", "imgMercedes3.jpg", "imgMercedes4.jpg", "imgMercedes5.jpg"],
         "mes_ano": "10/2018",
@@ -39,12 +39,12 @@ let carros = [
         "portas": 3,
         "lugares": 2
     },
-    { 
-        "id": 3, 
-        "marca": "Nissan", 
-        "modelo": "Micra Tekna", 
-        "ano": 2017, 
-        "kms": 114000, 
+    {
+        "id": 3,
+        "marca": "Nissan",
+        "modelo": "Micra Tekna",
+        "ano": 2017,
+        "kms": 114000,
         "preco": 16900,
         "imagens": ["Nissan_Micra_tekna.jpg", "imgMicra1.jpg", "imgMicra2.jpg", "imgMicra3.jpg", "imgMicra4.jpg"],
         "mes_ano": "04/2017",
@@ -59,12 +59,12 @@ let carros = [
         "portas": 5,
         "lugares": 4
     },
-    { 
-        "id": 4, 
-        "marca": "Opel", 
-        "modelo": "Astra 1.3 CDTi Selection S/S", 
-        "ano": 2014, 
-        "kms": 82000, 
+    {
+        "id": 4,
+        "marca": "Opel",
+        "modelo": "Astra 1.3 CDTi Selection S/S",
+        "ano": 2014,
+        "kms": 82000,
         "preco": 11900,
         "imagens": ["imgAstra2.jpg", "imgAstra1.jpg", "imgAstra3.jpg", "imgAstra4.jpg", "imgAstra5.jpg"],
         "mes_ano": "10/2014",
@@ -78,6 +78,26 @@ let carros = [
         "transmissao": "Manual",
         "portas": 5,
         "lugares": 5
+    }
+    {
+        "id": 5,
+        "marca": "Bmw",
+        "modelo": "BMW M4 Competition Cabrio LCI",
+        "ano": 2018,
+        "kms": 18000,
+        "preco": 79990,
+        "imagens": ["imgBmw2.jpg", "imgBmw1.jpg", "imgBmw3.jpg", "imgBmw4.jpg", "imgBmw5.jpg"],
+        "mes_ano": "01/2018",
+        "cilindrada": 3000,
+        "potencia": 450,
+        "vel_max": 330,
+        "combustivel": "Gasolina",
+        "capacidade": 60,
+        "cor_ext": "Azul",
+        "cor_int": "Preto",
+        "transmissao": "Automatica",
+        "portas": 3,
+        "lugares": 4
     }
 ];
 
