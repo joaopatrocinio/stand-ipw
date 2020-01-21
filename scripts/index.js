d86d93f36cf6c9ec5f70995cae62feb5a53e3f9e
Nelson Filipe Freire Antunes <nantunes@namacbook.eduroam.intranet.ips.pt>
2020-01-21 09:07:19 +0000

    w

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,9 @@ var captions = document.querySelectorAll("figcaption").forEach(function (caption
         window.location.href = "procura.html?query=" + caption.innerHTML;
     });
 });
+
+var images = document.querySelectorAll(".marca>img").forEach(function (image) {
+    image.addEventListener('click', function (event) {
+        window.location.href = "procura.html?query=" + image.nextElementSibling.innerHTML;
+    });
+});
